9abe44737401c70c69da676479782eeb7e16f6ac
amriteshonly4u <amriteshonly4u@gmail.com>
2019-11-28 09:46:16 +0530

    1. Seeding Done 2. Migration Done 3. Bootstrap Added 4. Model and Controller Created with resources Controller

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\EmployeeModel;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\EmployeeModel  $employeeModel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(EmployeeModel $employeeModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\EmployeeModel  $employeeModel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(EmployeeModel $employeeModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\EmployeeModel  $employeeModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, EmployeeModel $employeeModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\EmployeeModel  $employeeModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(EmployeeModel $employeeModel)
+    {
+        //
+    }
+}
---
+++ database/seeds/EmployeesSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class EmployeesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@mailinator.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
